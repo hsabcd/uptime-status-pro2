3ef4423c98e7b8fab7b4b71d64c7c11a2a5640e3
hsabcd <hsz@outlook.jp>
2025-03-02 16:03:29 +0800

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,16 +1,16 @@
 window.Config = {
 
   // 显示标题
-  SiteName: '咪咪爱空间监控站',
+  SiteName: 'MJJ网站监控',
 
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
-    'ur2849375-55cc3b6b358c15a1629b9230',
+    'ur2828404-32ec189c0d47d45b7f0a207a',
   ],
 
   // 日志天数
-  CountDays: 60,
+  CountDays: 30,
 
   // 是否显示检测站点的链接
   ShowLink: true,
@@ -25,11 +25,11 @@ window.Config = {
   Navi: [
     {
       text: '主页',
-      url: 'https://mmmc.us.kg'
+      url: 'https://www.xh.sd'
     },
     {
       text: 'Github',
-      url: 'https://github.com/hsabcd/uptime-status'
+      url: 'https://github.com/caogens/uptime-status-pro'
     },
   ],
 };
